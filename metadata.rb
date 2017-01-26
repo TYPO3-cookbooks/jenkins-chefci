b377f9c16ce18e7c1d5e2aa3cfcc4a3e50c536f6
Steffen Gebert <steffen.gebert@typo3.org>
2017-01-26 08:03:42 +0100

    [TASK] Align metadata contents

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
-name 'jenkins-chefci'
-maintainer 'Steffen Gebert'
+name             'jenkins-chefci'
+maintainer       'Steffen Gebert'
 maintainer_email 'steffen.gebert@typo3.org'
-license 'Apache 2.0'
-description 'Jenkins-based Cookbook Pipeline'
+license          'Apache 2.0'
+description      'Jenkins-based Cookbook Pipeline'
 long_description 'Sets up a Chef CI/CD based on Jenkins Pipelines.'
-version '0.1.0'
+version          '0.1.0'
 
-issues_url 'https://github.com/TYPO3-cookbooks/jenkins-chefci/issues' if respond_to?(:issues_url)
-source_url 'https://github.com/TYPO3-cookbooks/jenkins-chefci' if respond_to?(:source_url)
+issues_url       'https://github.com/TYPO3-cookbooks/jenkins-chefci/issues' if respond_to?(:issues_url)
+source_url       'https://github.com/TYPO3-cookbooks/jenkins-chefci' if respond_to?(:source_url)
