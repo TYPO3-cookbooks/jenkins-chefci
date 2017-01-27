f4e7c7b51e17960e8260822ae2e84693217c85e9
Steffen Gebert <steffen.gebert@typo3.org>
2017-01-27 20:05:21 +0100

    [TASK] Allow wrapper cookbooks to set up Jenkins auth
    If node.run_state[:jenkins_private_key] is already filled,
    this cookbook will not handle Jenkins authentication.

--- recipes/_jenkins_chef_user.rb
+++ recipes/_jenkins_chef_user.rb
@@ -1,38 +1,49 @@
 =begin
 #<
-Sets up Authentication for Chef-Client
+Sets up Authentication for Chef-Client towards Jenkins
+
+This is one possbile implementation for handling this.
+For more details, see the Jenkins cookbook documentation.
+
+Nothing will be done, if your wrapper cookbook already defined
+node.run_state[:jenkins_private_key]
 #>
 =end
 
 require 'openssl'
 require 'net/ssh'
 
-# defaults to /etc/chef-jenkins-api.key
-jenkins_chefci_private_key_path = node['jenkins_chefci']['jenkins_private_key_path']
 
-begin
-  Chef::Log.info 'Trying to read private key from ' + jenkins_chefci_private_key_path + ' for the chef user in jenkins'
-  key = OpenSSL::PKey::RSA.new File.read(jenkins_chefci_private_key_path)
-  Chef::Log.info 'Successfully read existing private key'
-rescue
-  key = OpenSSL::PKey::RSA.new 2048
-  Chef::Log.info 'Generating new key pair for the chef user in jenkins in ' + jenkins_chefci_private_key_path
+unless node.run_state[:jenkins_private_key]
 
-  file jenkins_chefci_private_key_path do
-    content key.to_pem
-    mode 0500
-    sensitive true
+  # defaults to /etc/chef-jenkins-api.key
+  key_path = node['jenkins_chefci']['jenkins_private_key_path']
+
+  begin
+    Chef::Log.info 'Trying to read private key from ' + key_path + ' for the chef user in jenkins'
+    key = OpenSSL::PKey::RSA.new File.read(key_path)
+    Chef::Log.info 'Successfully read existing private key'
+  rescue
+    key = OpenSSL::PKey::RSA.new 2048
+    Chef::Log.info 'Generating new key pair for the chef user in jenkins in ' + key_path
+
+    file key_path do
+      content key.to_pem
+      mode 0500
+      sensitive true
+    end
   end
+
+  public_key = [key.ssh_type, [key.to_blob].pack('m0'), 'auto-generated key'].join(' ')
+
+  # Create the Jenkins user with the public key
+  jenkins_user 'chef' do
+    id 'chef@' + Chef::Config[:node_name]
+    full_name 'Chef Client'
+    public_keys [public_key]
+  end
+
+  # Set the private key on the Jenkins executor
+  node.run_state[:jenkins_private_key] = key.to_pem
+
 end
-
-public_key = [key.ssh_type, [key.to_blob].pack('m0'), 'auto-generated key'].join(' ')
-
-# Create the Jenkins user with the public key
-jenkins_user 'chef' do
-  id 'chef@' + Chef::Config[:node_name]
-  full_name 'Chef Client'
-  public_keys [public_key]
-end
-
-# Set the private key on the Jenkins executor
-node.run_state[:jenkins_private_key] = key.to_pem
