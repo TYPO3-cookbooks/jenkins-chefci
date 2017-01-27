4fdac761e057baf0095fdab5381190644e51cbf8
Steffen Gebert <steffen.gebert@typo3.org>
2017-01-27 10:42:22 +0100

    [TASK] Use thor-scmversion

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'steffen.gebert@typo3.org'
 license          'Apache 2.0'
 description      'Jenkins-based Cookbook Pipeline'
 long_description 'Sets up a Chef CI/CD based on Jenkins Pipelines.'
-version          '0.1.0'
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.0'
 
 issues_url       'https://github.com/TYPO3-cookbooks/jenkins-chefci/issues' if respond_to?(:issues_url)
 source_url       'https://github.com/TYPO3-cookbooks/jenkins-chefci' if respond_to?(:source_url)
