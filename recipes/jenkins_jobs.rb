f7d99ec43dff37518ad2757c4067fc526ee79df4
Steffen Gebert <steffen.gebert@typo3.org>
2017-01-29 16:06:53 +0100

    [TASK] Use neutral name github-token for credentials

--- recipes/jenkins_jobs.rb
+++ recipes/jenkins_jobs.rb
@@ -1,18 +1,15 @@
 node.run_state[:jenkins_chefci_github_token] ||= node['jenkins_chefci']['github_credentials']['token']
 
 github_login = node['jenkins_chefci']['github_credentials']['login']
-credentials_id = nil
 
 if github_login
-  credentials_id = ['github', github_login, 'token'].join '-'
-
   jenkins_password_credentials github_login do
-    id credentials_id
+    id 'github-token'
     password node.run_state[:jenkins_chefci_github_token]
     description 'Github API token'
   end
 end
 
 jenkins_chefci_github_organization_job node['jenkins_chefci']['github_organization'] do
-  credentials_id credentials_id
+  credentials_id 'github-token' if github_login
 end
