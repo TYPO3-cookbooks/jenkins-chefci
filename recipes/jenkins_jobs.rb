7d512b44140a68517c8538107e83e09c86827336
Steffen Gebert <steffen.gebert@typo3.org>
2017-01-29 13:26:39 +0100

    [BUGFIX] Only set up credentials if they are specified

--- recipes/jenkins_jobs.rb
+++ recipes/jenkins_jobs.rb
@@ -1,12 +1,16 @@
 node.run_state[:jenkins_chefci_github_token] ||= node['jenkins_chefci']['github_credentials']['token']
 
 github_login = node['jenkins_chefci']['github_credentials']['login']
-credentials_id = ['github', github_login, 'token'].join '-'
+credentials_id = nil
 
-jenkins_password_credentials github_login do
-  id credentials_id
-  password node.run_state[:jenkins_chefci_github_token]
-  description 'Github API token'
+if github_login
+  credentials_id = ['github', github_login, 'token'].join '-'
+
+  jenkins_password_credentials github_login do
+    id credentials_id
+    password node.run_state[:jenkins_chefci_github_token]
+    description 'Github API token'
+  end
 end
 
 jenkins_chefci_github_organization_job node['jenkins_chefci']['github_organization'] do
