6431b6874a92a59e2e6ab9489e636f30621412da
Steffen Gebert <steffen.gebert@typo3.org>
2017-02-05 18:32:06 +0100

    [TASK] Improve attribute docs

--- attributes/local-testing.rb
+++ attributes/local-testing.rb
@@ -5,7 +5,8 @@
 # node.run_state[:jenkins_chefci_github_login] instead
 default['jenkins_chefci']['github_credentials']['login'] = nil
 
-#<> You *can* set this via an attribute (used in .kitchen.yml), but it is more safe to set
+#<> The token associated with the specified github user.
+# You *can* set this via an attribute (used in .kitchen.yml), but it is more safe to set
 # node.run_state[:jenkins_chefci_github_token] instead
 default['jenkins_chefci']['github_credentials']['token'] = nil
 
