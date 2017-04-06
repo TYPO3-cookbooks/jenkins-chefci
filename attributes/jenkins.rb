e8e4c14db57eb9f4676e55c8fc829f0f5ad9bab5
Steffen Gebert <steffen.gebert@typo3.org>
2017-04-06 20:58:05 +0200

    [TASK] Plugin updates #patch

--- attributes/jenkins.rb
+++ attributes/jenkins.rb
@@ -18,26 +18,26 @@ default['jenkins_chefci']['jenkins_plugins'] = %w(
   ant:1.4
   antisamy-markup-formatter:1.5
   authentication-tokens:1.3
-  blueocean:1.0.0-rc3
+  blueocean:1.0.0
   blueocean-autofavorite:0.6
-  blueocean-commons:1.0.0-rc3
-  blueocean-config:1.0.0-rc3
-  blueocean-dashboard:1.0.0-rc3
+  blueocean-commons:1.0.0
+  blueocean-config:1.0.0
+  blueocean-dashboard:1.0.0
   blueocean-display-url:1.5.1
-  blueocean-events:1.0.0-rc3
-  blueocean-git-pipeline:1.0.0-rc3
-  blueocean-github-pipeline:1.0.0-rc3
-  blueocean-i18n:1.0.0-rc3
-  blueocean-jwt:1.0.0-rc3
-  blueocean-personalization:1.0.0-rc3
-  blueocean-pipeline-api-impl:1.0.0-rc3
+  blueocean-events:1.0.0
+  blueocean-git-pipeline:1.0.0
+  blueocean-github-pipeline:1.0.0
+  blueocean-i18n:1.0.0
+  blueocean-jwt:1.0.0
+  blueocean-personalization:1.0.0
+  blueocean-pipeline-api-impl:1.0.0
   blueocean-pipeline-editor:0.2.0
-  blueocean-rest:1.0.0-rc3
-  blueocean-rest-impl:1.0.0-rc3
-  blueocean-web:1.0.0-rc3
-  bouncycastle-api:2.16.0
+  blueocean-rest:1.0.0
+  blueocean-rest-impl:1.0.0
+  blueocean-web:1.0.0
+  bouncycastle-api:2.16.1
   branch-api:2.0.8
-  buildtriggerbadge:2.8
+  buildtriggerbadge:2.8.1
   clone-workspace-scm:0.6
   cloudbees-folder:6.0.3
   credentials:2.1.13
@@ -53,9 +53,9 @@ default['jenkins_chefci']['jenkins_plugins'] = %w(
   git:3.2.0
   git-client:2.4.1
   git-server:1.7
-  github:1.26.1
+  github:1.26.2
   github-api:1.85
-  github-branch-source:2.0.4
+  github-branch-source:2.0.5
   github-oauth:0.25
   github-organization-folder:1.6
   greenballs:1.15
@@ -79,12 +79,13 @@ default['jenkins_chefci']['jenkins_plugins'] = %w(
   pipeline-graph-analysis:1.3
   pipeline-input-step:2.5
   pipeline-milestone-step:1.3.1
-  pipeline-model-api:1.1.1
-  pipeline-model-definition:1.1.1
-  pipeline-model-extensions:1.1.1
+  pipeline-model-api:1.1.2
+  pipeline-model-declarative-agent:1.1.1
+  pipeline-model-definition:1.1.2
+  pipeline-model-extensions:1.1.2
   pipeline-rest-api:2.6
   pipeline-stage-step:2.2
-  pipeline-stage-tags-metadata:1.1.1
+  pipeline-stage-tags-metadata:1.1.2
   pipeline-stage-view:2.6
   piwikanalytics:1.2.0
   plain-credentials:1.4
@@ -96,7 +97,7 @@ default['jenkins_chefci']['jenkins_plugins'] = %w(
   sse-gateway:1.15
   ssh-credentials:1.13
   structs:1.6
-  token-macro:2.0
+  token-macro:2.1
   variant:1.1
   warnings:4.60
   windows-slaves:1.3.1
