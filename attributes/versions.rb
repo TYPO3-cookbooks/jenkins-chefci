94db84fa74f3c5bae10784747f5cbaea45a9aa37
Steffen Gebert <steffen.gebert@typo3.org>
2017-04-17 21:14:34 +0200

    [TASK] Update Chef to 12.19.36 / ChefDK to 1.3.40

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -1,5 +1,5 @@
 #<> Configures the ChefDK version to be installed - see https://github.com/chef/chef-dk
-default['jenkins_chefci']['chefdk_version'] = '1.0.3-1'
+default['jenkins_chefci']['chefdk_version'] = '1.3.40-1'
 
 #<> Configures the version of Chef to use for test-kitchen runs
-default['jenkins_chefci']['kitchen']['chef_version'] = '12.5.1'
+default['jenkins_chefci']['kitchen']['chef_version'] = '12.19.36'
