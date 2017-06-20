31ede5c4d5e5472a0442904bcc055e014c69d2c5
Steffen Gebert <steffen.gebert@typo3.org>
2017-06-20 21:51:05 +0200

    [TASK] Update to Chef 12.21.1

--- attributes/versions.rb
+++ attributes/versions.rb
@@ -2,4 +2,4 @@
 default['jenkins_chefci']['chefdk_version'] = '1.0.3-1'
 
 #<> Configures the version of Chef to use for test-kitchen runs
-default['jenkins_chefci']['kitchen']['chef_version'] = '12.20.3'
+default['jenkins_chefci']['kitchen']['chef_version'] = '12.21.1'
