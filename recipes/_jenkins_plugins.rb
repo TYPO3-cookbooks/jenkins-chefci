dda28d1ddc2baadbe1ceb12065f119056a76b36b
Steffen Gebert <steffen.gebert@typo3.org>
2017-07-16 14:19:11 +0200

    [BUGFIX] Don't write jenkins-plugin-last-state file to file_cache_path
    As described in someara/kitchen-dokken#118, the contents of
    Chef::Config[:file_cache_path] are persistent on the host's file system.
    
    This leads to the unexpected effect that Jenkins is not reliably restarted
    after plugin installation, if the previous run of kitchen-dokken had the
    same plugins installated.
    
    Therefore, put the file into a directory inside the container, which is
    reliably destroyed together with the container.

--- recipes/_jenkins_plugins.rb
+++ recipes/_jenkins_plugins.rb
@@ -7,7 +7,7 @@ node['jenkins_chefci']['jenkins_plugins'].each_with_index do |plugin_with_versio
 end
 
 # we want to restart Jenkins if any plugin changed
-file File.join(Chef::Config[:file_cache_path], 'jenkins-plugins-last-state') do
+file File.join(node['jenkins']['master']['home'], 'jenkins-plugins-last-state') do
   content node['jenkins_chefci']['jenkins_plugins'].map{|k,v| "#{k}:#{v}"}.sort.join("\n")
   notifies :execute, 'jenkins_command[safe-restart]', :immediately
 end
